605269b335fdd67210e8904be1866f2f6d6ac51b
sm <sebastian.molerus@gmail.com>
2023-09-10 14:09:34 +0200

    moved 16bit LD to other file

--- src/cpu/tests/instructions/load/test_ld_16.cpp
+++ src/cpu/tests/instructions/load/test_ld_16.cpp
@@ -3,7 +3,16 @@
 #include <gtest/gtest.h>
 #include <translator.hpp>
 
-TEST(test_load_16bit, ld_BC_n16)
+uint16_t read_word_from_stack(CpuData const &data)
+{
+    uint16_t result = data.m_memory[data.SP.u16 + 1];
+    result <<= 8;
+    result |= data.m_memory[data.SP.u16];
+    return result;
+}
+
+// 0x01
+TEST(test_load_16bit, LD_BC_n16)
 {
     std::string assembly{R"(
         LD BC, 0x16C7
@@ -37,7 +46,8 @@ TEST(test_load_16bit, ld_BC_n16)
     ASSERT_EQ(expected_data[3].BC.hi, 0x5);
 }
 
-TEST(test_load_16bit, ld_DE_n16)
+// 0x11
+TEST(test_load_16bit, LD_DE_n16)
 {
     std::string assembly{R"(
         LD DE, 0x157F
@@ -71,7 +81,8 @@ TEST(test_load_16bit, ld_DE_n16)
     ASSERT_EQ(expected_data[3].DE.hi, 0x8);
 }
 
-TEST(test_load_16bit, ld_HL_n16)
+// 0x21
+TEST(test_load_16bit, LD_HL_n16)
 {
     std::string assembly{R"(
         LD HL, 0xF50F
@@ -105,7 +116,8 @@ TEST(test_load_16bit, ld_HL_n16)
     ASSERT_EQ(expected_data[3].HL.hi, 0xC);
 }
 
-TEST(test_load_16bit, ld_SP_n16)
+// 0x31
+TEST(test_load_16bit, LD_SP_n16)
 {
     std::string assembly{R"(
         LD SP, 0x7800
@@ -137,4 +149,307 @@ TEST(test_load_16bit, ld_SP_n16)
     ASSERT_EQ(expected_data[3].SP.u16, 0x87B);
     ASSERT_EQ(expected_data[3].SP.lo, 0x7B);
     ASSERT_EQ(expected_data[3].SP.hi, 0x8);
+}
+
+// 0xF8
+TEST(test_load_16bit, LD_HL_SP_e8)
+{
+    std::string assembly{R"(
+        LD SP, 0x3E
+        LD HL, SP + 0x23
+        LD SP, 0xFFFF
+        LD HL, SP + 0x1
+        LD SP, 0x5
+        LD HL, SP + 0x81
+    )"};
+    auto opcodes = translate(assembly);
+    Cpu cpu{opcodes};
+
+    std::vector<CpuData> expected_data;
+    auto f = [&expected_data](const CpuData &d, const Opcode &op) {
+        if (op.hex == get_opcode("LD HL, SP + e8").hex)
+            expected_data.push_back(d);
+    };
+    cpu.register_function_callback(f);
+    cpu.process();
+
+    ASSERT_EQ(expected_data.size(), 3);
+
+    for (auto &data : expected_data)
+    {
+        ASSERT_FALSE(data.is_flag_set(CpuData::FLAG_Z));
+        ASSERT_FALSE(data.is_flag_set(CpuData::FLAG_N));
+    }
+
+    ASSERT_EQ(expected_data[0].HL.u16, 97);
+    ASSERT_TRUE(expected_data[0].is_flag_set(CpuData::FLAG_H));
+    ASSERT_FALSE(expected_data[0].is_flag_set(CpuData::FLAG_C));
+
+    ASSERT_EQ(expected_data[1].HL.u16, 0);
+    ASSERT_TRUE(expected_data[1].is_flag_set(CpuData::FLAG_H));
+    ASSERT_TRUE(expected_data[1].is_flag_set(CpuData::FLAG_C));
+
+    ASSERT_EQ(expected_data[2].HL.u16, 4);
+}
+
+// 0xF8
+TEST(test_load_16bit, LD_HL_SP_e8_half_carry)
+{
+    std::string assembly{R"(
+        LD SP, 0x9
+        LD HL, SP + 0x10
+        LD SP, 0xA
+        LD HL, SP + 0xC
+    )"};
+    auto opcodes = translate(assembly);
+    Cpu cpu{opcodes};
+
+    std::vector<CpuData> expected_data;
+    auto f = [&expected_data](const CpuData &d, const Opcode &op) {
+        if (op.hex == get_opcode("LD HL, SP + e8").hex)
+            expected_data.push_back(d);
+    };
+    cpu.register_function_callback(f);
+    cpu.process();
+
+    ASSERT_EQ(expected_data.size(), 2);
+
+    ASSERT_FALSE(expected_data[0].is_flag_set(CpuData::FLAG_H));
+    ASSERT_TRUE(expected_data[1].is_flag_set(CpuData::FLAG_H));
+}
+
+// 0xF8
+TEST(test_load_16bit, LD_HL_SP_e8_carry)
+{
+    std::string assembly{R"(
+        LD SP, 0xFFFE
+        LD HL, SP + 0x1
+        LD SP, 0xFFFF
+        LD HL, SP + 0x1
+    )"};
+    auto opcodes = translate(assembly);
+    Cpu cpu{opcodes};
+
+    std::vector<CpuData> expected_data;
+    auto f = [&expected_data](const CpuData &d, const Opcode &op) {
+        if (op.hex == get_opcode("LD HL, SP + e8").hex)
+            expected_data.push_back(d);
+    };
+    cpu.register_function_callback(f);
+    cpu.process();
+
+    ASSERT_EQ(expected_data.size(), 2);
+
+    ASSERT_FALSE(expected_data[0].is_flag_set(CpuData::FLAG_C));
+    ASSERT_TRUE(expected_data[1].is_flag_set(CpuData::FLAG_C));
+}
+
+// 0x08
+TEST(test_load_16bit, LD_Ia16I_SP)
+{
+    std::string assembly{R"(
+        LD SP, 0xBBAA
+        LD [0x1], SP
+    )"};
+    auto opcodes = translate(assembly);
+    Cpu cpu{opcodes};
+
+    CpuData expected_data;
+    auto f = [&expected_data](const CpuData &d, const Opcode &op) { expected_data = d; };
+    cpu.register_function_callback(f);
+    cpu.process();
+
+    ASSERT_EQ(expected_data.m_memory[0x1], 0xAA);
+    ASSERT_EQ(expected_data.m_memory[0x2], 0xBB);
+}
+
+// 0xF9
+TEST(test_load_16bit, LD_SP_HL)
+{
+    std::string assembly{R"(
+        LD HL, 0xABCD
+        LD SP, HL
+    )"};
+    auto opcodes = translate(assembly);
+    Cpu cpu{opcodes};
+
+    CpuData expected_data;
+    auto f = [&expected_data](const CpuData &d, const Opcode &op) { expected_data = d; };
+    cpu.register_function_callback(f);
+    cpu.process();
+
+    ASSERT_EQ(expected_data.HL.u16, 0xABCD);
+    ASSERT_EQ(expected_data.SP.u16, expected_data.HL.u16);
+}
+
+// 0xC5
+TEST(test_load_16bit, PUSH_BC)
+{
+    std::string assembly{R"(
+        LD SP, 0xA
+        LD BC, 0x548A
+        PUSH BC
+    )"};
+    auto opcodes = translate(assembly);
+    Cpu cpu{opcodes};
+
+    CpuData expected_data;
+    auto f = [&expected_data](const CpuData &d, const Opcode &op) { expected_data = d; };
+    cpu.register_function_callback(f);
+    cpu.process();
+
+    // Is stack decremented by 2 bytes
+    ASSERT_EQ(expected_data.SP.u16, 8);
+    ASSERT_EQ(read_word_from_stack(expected_data), 0x548A);
+}
+
+// 0xD5
+TEST(test_load_16bit, PUSH_DE)
+{
+    std::string assembly{R"(
+        LD SP, 0xBB
+        LD DE, 0xAAFF
+        PUSH DE
+    )"};
+    auto opcodes = translate(assembly);
+    Cpu cpu{opcodes};
+
+    CpuData expected_data;
+    auto f = [&expected_data](const CpuData &d, const Opcode &op) { expected_data = d; };
+    cpu.register_function_callback(f);
+    cpu.process();
+
+    // Is stack decremented by 2 bytes
+    ASSERT_EQ(expected_data.SP.u16, 0xBB - 2);
+    ASSERT_EQ(read_word_from_stack(expected_data), 0xAAFF);
+}
+
+// 0xE5
+TEST(test_load_16bit, PUSH_HL)
+{
+    std::string assembly{R"(
+        LD SP, 0xAB
+        LD HL, 0x1234
+        PUSH HL
+    )"};
+    auto opcodes = translate(assembly);
+    Cpu cpu{opcodes};
+
+    CpuData expected_data;
+    auto f = [&expected_data](const CpuData &d, const Opcode &op) { expected_data = d; };
+    cpu.register_function_callback(f);
+    cpu.process();
+
+    // Is stack decremented by 2 bytes
+    ASSERT_EQ(expected_data.SP.u16, 0xAB - 2);
+    ASSERT_EQ(read_word_from_stack(expected_data), 0x1234);
+}
+
+// 0xF5
+TEST(test_load_16bit, PUSH_AF)
+{
+    std::string assembly{R"(
+        LD SP, 0x100
+        LD DE, 0xBD00
+        LD A, D
+        PUSH AF
+    )"};
+    auto opcodes = translate(assembly);
+    Cpu cpu{opcodes};
+
+    CpuData expected_data;
+    auto f = [&expected_data](const CpuData &d, const Opcode &op) { expected_data = d; };
+    cpu.register_function_callback(f);
+    cpu.process();
+
+    // Is stack decremented by 2 bytes
+    ASSERT_EQ(expected_data.SP.u16, 0x0100 - 2);
+    ASSERT_EQ(read_word_from_stack(expected_data), 0xBD00);
+}
+
+// 0xC1
+TEST(test_load_16bit, pop_BC)
+{
+    std::string assembly{R"(
+        LD SP, 0x12
+        LD HL, 0xF0F0
+        PUSH HL
+        POP BC
+    )"};
+    auto opcodes = translate(assembly);
+    Cpu cpu{opcodes};
+
+    CpuData expected_data;
+    auto f = [&expected_data](const CpuData &d, const Opcode &op) { expected_data = d; };
+    cpu.register_function_callback(f);
+    cpu.process();
+
+    ASSERT_EQ(expected_data.SP.u16, 0x12);
+    ASSERT_EQ(expected_data.BC.u16, 0xF0F0);
+}
+
+// 0xD1
+TEST(test_load_16bit, pop_DE)
+{
+    std::string assembly{R"(
+        LD SP, 0x89
+        LD HL, 0x1561
+        PUSH HL
+        POP DE
+    )"};
+    auto opcodes = translate(assembly);
+    Cpu cpu{opcodes};
+
+    CpuData expected_data;
+    auto f = [&expected_data](const CpuData &d, const Opcode &op) { expected_data = d; };
+    cpu.register_function_callback(f);
+    cpu.process();
+
+    ASSERT_EQ(expected_data.SP.u16, 0x89);
+    ASSERT_EQ(expected_data.DE.u16, 0x1561);
+}
+
+// 0xE1
+TEST(test_load_16bit, pop_HL)
+{
+    std::string assembly{R"(
+        LD SP, 0x99
+        LD BC, 0xBABE
+        PUSH BC
+        POP HL
+    )"};
+    auto opcodes = translate(assembly);
+    Cpu cpu{opcodes};
+
+    CpuData expected_data;
+    auto f = [&expected_data](const CpuData &d, const Opcode &op) { expected_data = d; };
+    cpu.register_function_callback(f);
+    cpu.process();
+
+    ASSERT_EQ(expected_data.SP.u16, 0x99);
+    ASSERT_EQ(expected_data.HL.u16, 0xBABE);
+}
+
+// 0xF1
+TEST(test_load_16bit, pop_AF)
+{
+    std::string assembly{R"(
+        LD SP, 0x303
+        LD DE, 0xBAFE
+        PUSH DE
+        POP AF
+    )"};
+    auto opcodes = translate(assembly);
+    Cpu cpu{opcodes};
+
+    CpuData expected_data;
+    auto f = [&expected_data](const CpuData &d, const Opcode &op) {
+        if (op.hex == 0xF1)
+            expected_data = d;
+    };
+    cpu.register_function_callback(f);
+    cpu.process();
+
+    ASSERT_EQ(expected_data.SP.u16, 0x303);
+    ASSERT_EQ(expected_data.AF.u16, 0xBAFE);
 }
