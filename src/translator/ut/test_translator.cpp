2cda6eac4c0a04dea928973813a0a559585eb444
SebastianMolerus <sebastian.molerus@gmail.com>
2023-12-11 19:12:30 +0100

    UT for translator to check jump instructions

--- src/translator/ut/test_translator.cpp
+++ src/translator/ut/test_translator.cpp
@@ -72,3 +72,110 @@ TEST(translator_test, translate_instructions_ld_3)
     ASSERT_EQ(machine_code[1], 0x00);
     ASSERT_EQ(machine_code[2], 0x01);
 }
+
+TEST(translator_test, jumps_2B_p1)
+{
+    std::string st{R"(
+        JR NZ, 0xAB
+        JR NC, 0x12
+    )"};
+
+    auto machine_code = translate(st);
+
+    ASSERT_EQ(machine_code.size(), 4);
+    ASSERT_EQ(machine_code[0], 0x20);
+    ASSERT_EQ(machine_code[1], 0xAB);
+    ASSERT_EQ(machine_code[2], 0x30);
+    ASSERT_EQ(machine_code[3], 0x12);
+}
+
+TEST(translator_test, jumps_2B_p2)
+{
+    std::string st{R"(
+        JR 0x78
+        JR Z, 0xA7
+        JR C, 0x1
+    )"};
+
+    auto machine_code = translate(st);
+
+    ASSERT_EQ(machine_code.size(), 6);
+    ASSERT_EQ(machine_code[0], 0x18);
+    ASSERT_EQ(machine_code[1], 0x78);
+
+    ASSERT_EQ(machine_code[2], 0x28);
+    ASSERT_EQ(machine_code[3], 0xA7);
+
+    ASSERT_EQ(machine_code[4], 0x38);
+    ASSERT_EQ(machine_code[5], 0x1);
+}
+
+TEST(translator_test, jumps_1B)
+{
+    std::string st{R"(
+        RET NZ
+        RET NC
+        RST $00
+        RST $10
+        RST $20
+        RST $30
+        RET Z
+        RET C
+        RET
+        RETI
+        JP HL
+        RST $08
+        RST $18
+        RST $28
+        RST $38
+    )"};
+
+    auto machine_code = translate(st);
+
+    ASSERT_EQ(machine_code.size(), 15);
+    ASSERT_EQ(machine_code[0], 0xC0);
+    ASSERT_EQ(machine_code[1], 0xD0);
+    ASSERT_EQ(machine_code[2], 0xC7);
+    ASSERT_EQ(machine_code[3], 0xD7);
+    ASSERT_EQ(machine_code[4], 0xE7);
+    ASSERT_EQ(machine_code[5], 0xF7);
+    ASSERT_EQ(machine_code[6], 0xC8);
+    ASSERT_EQ(machine_code[7], 0xD8);
+    ASSERT_EQ(machine_code[8], 0xC9);
+    ASSERT_EQ(machine_code[9], 0xD9);
+    ASSERT_EQ(machine_code[10], 0xE9);
+    ASSERT_EQ(machine_code[11], 0xCF);
+    ASSERT_EQ(machine_code[12], 0xDF);
+    ASSERT_EQ(machine_code[13], 0xEF);
+    ASSERT_EQ(machine_code[14], 0xFF);
+}
+
+TEST(translator_test, jumps_3B)
+{
+    std::string st{R"(
+        JP NZ, 0x76
+        JP NC, 0x123
+        JP 0x7896
+        CALL NZ, 0x1
+        CALL NC, 0x320
+        JP Z, 0x0
+        JP C, 0x123F
+        CALL Z, 0xFFFF
+        CALL C, 0xFFF
+        CALL 0xFF
+    )"};
+
+    auto machine_code = translate(st);
+
+    ASSERT_EQ(machine_code[0], 0xC2);
+    ASSERT_EQ(machine_code[1], 0x76);
+    ASSERT_EQ(machine_code[2], 0x00);
+
+    ASSERT_EQ(machine_code[3], 0xD2);
+    ASSERT_EQ(machine_code[4], 0x23);
+    ASSERT_EQ(machine_code[5], 0x01);
+
+    ASSERT_EQ(machine_code[6], 0xC3);
+    ASSERT_EQ(machine_code[7], 0x96);
+    ASSERT_EQ(machine_code[8], 0x78);
+}
