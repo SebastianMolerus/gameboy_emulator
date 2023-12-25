36ef2e9acf4dbb854e1b146a21b4af58d69a2ce6
sm <sebastian.molerus@gmail.com>
2023-12-25 22:12:52 +0100

    SUB finished

---
+++ src/cpu/ut/big_tests/cpu/test_sub.cpp
@@ -0,0 +1,16 @@
+#include "../big_tests_utils.h"
+
+namespace
+{
+
+std::array<uint8_t, 9> const sub_opcodes{0x90, 0x91, 0x92, 0x93, 0x94, 0x95, 0x96, 0x97, 0xD6};
+
+} // namespace
+
+TEST(test_cpu_BIG, test_all_sub)
+{
+    for (auto hex : sub_opcodes)
+    {
+        validate_opcode(hex);
+    }
+}
