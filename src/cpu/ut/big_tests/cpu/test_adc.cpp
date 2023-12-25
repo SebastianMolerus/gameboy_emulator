b1ed185c71a9c3e15f1de4340526819a92f3a3e6
sm <sebastian.molerus@gmail.com>
2023-12-25 22:00:36 +0100

    ADC finished

---
+++ src/cpu/ut/big_tests/cpu/test_adc.cpp
@@ -0,0 +1,15 @@
+#include "../big_tests_utils.h"
+
+namespace
+{
+std::array<uint8_t, 9> const adc_opcodes{0x88, 0x89, 0x8A, 0x8B, 0x8C, 0x8D, 0x8E, 0x8F, 0xCE};
+
+} // namespace
+
+TEST(test_cpu_BIG, test_all_adc)
+{
+    for (auto hex : adc_opcodes)
+    {
+        validate_opcode(hex);
+    }
+}
